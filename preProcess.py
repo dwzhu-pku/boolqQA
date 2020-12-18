e04b3dbb62f01eb329901af2b8a0c23883352256
zdwls <52273452+zdwls@users.noreply.github.com>
2020-12-18 12:07:09 +0800

    为方便torchtext处理，利用本模块将jsonl转成json格式

---
+++ preProcess.py
@@ -0,0 +1,14 @@
+"""
+@Author: Dawei Zhu
+@Date: 2020-12-18
+@Description: 为方便torchtext处理，将jsonl转成json格式
+"""
+
+import jsonlines
+import json
+
+with jsonlines.open("./train.json", "w") as wfd:
+    with open("./train.jsonl", "r", encoding='utf-8') as rfd:
+        for data in rfd:
+            data = json.loads(data)
+            wfd.write(data)
