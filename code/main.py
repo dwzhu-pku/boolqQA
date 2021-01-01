a9ca19f23e41a688cd05206aecb0e224e97f1449
wzr <844570862@qq.com>
2021-01-01 19:49:23 +0800

    增加with title和inference

--- code/main.py
+++ code/main.py
@@ -11,14 +11,18 @@ from classifier import classify
 
 if __name__ == "__main__":
 
-    pattern = 'roberta'
+    pattern = 'bimpm'
+    print('pattern: ',pattern)
+
     config = classify(
         pattern=pattern,
-        epoch_num=30,lr=8e-6,batch_size =16,patience = 2,
-        device= torch.device("cuda:7" if torch.cuda.is_available() else "cpu"),
+        with_title = True,#with title与否，只是简单将title加到passage之前
+        epoch_num=30,lr=5e-4,batch_size =32,patience = 4,
+        device= torch.device("cuda:6" if torch.cuda.is_available() else "cpu"),
         GLOVE_PATH = "../datafile/glove.6B.100d.txt"
     )#设置device,在nn.module层面设置to(self.device))
     config.train()
     config.eval(need_load=False)
     config.eval(need_load=True)
+    config.inference()
     
