7ca447d8154331afe5b3f309d10f4899f8efb065
zdwls <1800012713@pku.edu.cn>
2021-01-01 11:56:36 +0000

    aaa

--- code/main.py
+++ code/main.py
@@ -11,11 +11,11 @@ from classifier import classify
 
 if __name__ == "__main__":
 
-    pattern = 'roberta'
+    pattern = 'lstm-attn'
     config = classify(
         pattern=pattern,
-        epoch_num=30,lr=8e-6,batch_size =16,patience = 2,
-        device= torch.device("cuda:7" if torch.cuda.is_available() else "cpu"),
+        epoch_num=20,lr=5e-4,batch_size =48,patience = 2,
+        device= torch.device("cuda:2" if torch.cuda.is_available() else "cpu"),
         GLOVE_PATH = "../datafile/glove.6B.100d.txt"
     )#设置device,在nn.module层面设置to(self.device))
     config.train()
